351b2a53cb7053b07a8a392da8c8ed9e4794f982
olcayhan <kolcayhan1@gmail.com>
2022-08-26 22:38:41 +0300

    adding comments

--- WordFinde/app.js
+++ WordFinde/app.js
@@ -1,39 +1,63 @@
+// declaration
 var rightanswer = [];
-var wronganswer =[];
+var wronganswer = [];
 var c =0
 var a=0;
 var temp =0;
 let word;
-let word2
+let word2;
 
+// load queries
 const generator = document.querySelector(".generator");
 const keyboard = document.querySelectorAll(".buttons");
 const box = document.querySelectorAll(".box")
 
-
-
-function getWord(){
-    return fetch("https://random-word-api.herokuapp.com/word?length=6")
-    .then(response => response.json())
-    .then(response => response[0]);
-    }
-
-
+// take the word for guess
 getWord().then(response => (
     word = response.toUpperCase(),
     word2 = word.split("")
     ));
 
+// keyboard listener
+for (let i=0; i < keyboard.length;i++) keyboard[i].addEventListener("click",(e)=>getKeyboard(e));
 
+// function for getting keyboard in the website
+function getKeyboard(e){
+    console.log(word)
+        
+    if(e.target.id != "del" && e.target.id != "ok"){
+        temp++;
+        if (temp<=6){
+            box[a].innerHTML = e.target.id;
+            a++;
+        }
+        else temp = 6;
+    }
+  
+    if (temp ==6 && e.target.id == "ok"){
+        temp -= 6;
+        contoller();
 
-function writeWord(){
+    }
+
+    if(e.target.id == "del" && temp <=6){
+        if (a>0 && temp>0){
+            a--;
+            temp--;
+            box[a].innerHTML = "";
+        } 
+    }
     
-
-for (let i=0; i < keyboard.length;i++){
-    keyboard[i].addEventListener("click",(e)=>getKeyboard(e));
-}
 }
 
+// function for getting word with API on the server.
+function getWord(){
+    return fetch("https://random-word-api.herokuapp.com/word?length=6")
+    .then(response => response.json())
+    .then(response => response[0]);
+}
+
+// function for checking gueesed letter true, false or letter in word but wrong box.
 function contoller(){ 
     
     rightanswer = [];
@@ -44,34 +68,27 @@ function contoller(){
             box[i+c].style.border = "#88B04B"
             rightanswer[i] = word[i];
             word2[i] = "";
-            
-           
         }
 
         else if(word2.includes(box[i+c].textContent)){
             box[i+c].style.background = "#EFC050"
             box[i+c].style.border = "#EFC050"
         }
-
         else{
             wronganswer.push(box[i+c].textContent);
             box[i+c].style.background = "#787c7e"
             box[i+c].style.border = "#787c7e"
-            for(let i =0;i<keyboard.length;i++){
-                if (wronganswer.includes(keyboard[i].id)){
-                    keyboard[i].style.background="#787c7e" ;
-                }
-            } 
-        } 
+            for(let i =0;i<keyboard.length;i++) if (wronganswer.includes(keyboard[i].id)) keyboard[i].style.background="#787c7e" ;
+        }
     }
     c +=6;
     console.log("Controller Working");
-    if (rightanswer.join('') == word || c == 30){
-        Overtester();
-    }
+    if (rightanswer.join('') == word || c == 30) Overtester();
+    
     
 }
 
+// function for checking player won or lose
 function Overtester(){
     console.log("Overtester Worked");
     
@@ -96,36 +113,3 @@ function Overtester(){
     btnTo.addEventListener("click",()=>{location.reload();
     })
 }
-
-function getKeyboard(e){
-    console.log(word)
-        
-    if(e.target.id != "del" && e.target.id != "ok"){
-        temp++;
-        
-        if (temp<=6){
-            box[a].innerHTML = e.target.id;
-            a++;
-        }
-        else{
-            temp = 6;
-        }
-    }
-    if (temp ==6 && e.target.id == "ok"){
-        contoller();
-        temp -= 6;
-    };
-    console.log(temp)
-    if(e.target.id == "del" && temp <=6){
-        if (a>0 && temp>0){
-            a--;
-            temp--;
-            box[a].innerHTML = "";
-        } 
-        
-    }
-}
-
-writeWord();
-
-
