53750bd38dfc0c36e51f5b1cbe38a75b22fd71f6
olcayhan <kolcayhan1@gmail.com>
2022-09-01 11:44:15 +0300

    generate mole different holes

--- Whac-a-mole/app.js
+++ Whac-a-mole/app.js
@@ -1,2 +1,24 @@
 const score = document.querySelector(".point");
+var hole = document.querySelectorAll(".hole");
+const sleep = ms => new Promise(r => setTimeout(r, ms));
+var mole = document.createElement("img");
 
+
+
+async function getMole(){
+    let random = Math.floor(Math.random() * 15)+1
+    mole.src = "images/animal.png";
+    document.getElementById(`hole${random}`).appendChild(mole);
+    await sleep(1000);
+    document.getElementById(`hole${random}`).removeChild(mole);
+
+}
+async function getTime(){
+   while(true){
+    await sleep(1000);
+    getMole();
+   }
+    
+}
+
+getTime();
